5d2e6bec0249fca485efd6b7a07b39dc3a5fe8c0
Artem Pupov <Tyuiu.PupovAA@outlook.com>
2025-10-14 22:51:18 +0500

    Task3

---
+++ Tyuiu.PupovAA.Sprint2.Task3.V21.Test/DataServiceTest.cs
@@ -0,0 +1,44 @@
+﻿using Tyuiu.PupovAA.Sprint2.Task3.V21.Lib;
+namespace Tyuiu.PupovAA.Sprint2.Task3.V21.Test
+{
+    [TestClass]
+    public sealed class DataServiceTest
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            DataService ds = new DataService();
+            double x = 2;
+            var y = ds.Calculate(x);
+            Assert.AreEqual(242, y);
+        }
+        
+        [TestMethod]
+        public void TestMethod2()
+        {
+            DataService ds = new DataService();
+            double x = 0;
+            var y = ds.Calculate(x);
+            Assert.AreEqual(0.833, y);
+        }
+        
+        [TestMethod]
+        public void TestMethod3()
+        {
+            DataService ds = new DataService();
+            double x = 1;
+            var y = ds.Calculate(x);
+            Assert.AreEqual(2, y);
+        }
+        
+        [TestMethod]
+        public void TestMethod4()
+        {
+            DataService ds = new DataService();
+            double x = -26;
+            var y = ds.Calculate(x);
+            Assert.AreEqual(-285.962, y);
+            
+        }
+    }
+}
---
+++ Tyuiu.PupovAA.Sprint2.Task3.V21/Program.cs
@@ -0,0 +1,31 @@
+﻿using Tyuiu.PupovAA.Sprint2.Task3.V21.Lib;
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        DataService ds = new DataService();
+        Console.Title = "Спринт #2 | Выполнил: Пупов А. А. | ИБКСб-25-1";
+        Console.WriteLine("***************************************************************************");
+        Console.WriteLine("* Спринт #2                                                               *");
+        Console.WriteLine("* Тема: Вложенные операторы if - else                                      *");
+        Console.WriteLine("* Задание #3                                                              *");
+        Console.WriteLine("* Вариант #21                                                             *");
+        Console.WriteLine("* Выполнил: Пупов Артём Андреевич | ИБКСб-25-1                            *");
+        Console.WriteLine("***************************************************************************");
+        Console.WriteLine("* УСЛОВИЕ:                                                                *");
+        Console.WriteLine("* Написать программу, которая вычисляет требуемое значение функции Y с     *");
+        Console.WriteLine("* использованием вложенных оператор if-else, где пользователь вводит значение *");
+        Console.WriteLine("* переменной X с клавиатуры. Округлить полученное значение до трех знаков *");
+        Console.WriteLine("* после запятой;                                                           *");
+        Console.WriteLine("***************************************************************************");
+        Console.WriteLine("* ИСХОДНЫЕ ДАННЫЕ:                                                        *");
+        Console.Write("Введите число x= ");
+        double x = Convert.ToDouble(Console.ReadLine());
+        Console.WriteLine("***************************************************************************");
+        Console.WriteLine("* РЕЗУЛЬТАТ:                                                              *");
+        Console.WriteLine("****************************************************************************");
+        var y = ds.Calculate(x);
+        Console.WriteLine("Значение функции Y = " + y);
+        Console.ReadLine();
+    }
+}
