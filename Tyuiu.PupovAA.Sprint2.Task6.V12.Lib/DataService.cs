df8394c73ca3e8c08a4e5cb69b28cccee9d30130
Artem Pupov <Tyuiu.PupovAA@outlook.com>
2025-10-15 09:06:42 +0500

    Task6

--- Tyuiu.PupovAA.Sprint2.Task6.V12.Lib/DataService.cs
+++ Tyuiu.PupovAA.Sprint2.Task6.V12.Lib/DataService.cs
@@ -42,7 +42,7 @@ namespace Tyuiu.PupovAA.Sprint2.Task6.V12.Lib
                 _ => throw new ArgumentException("Некорректный номер месяца")
             };
 
-            return $"{daysInPrevMonth}. {prevMonth}. {prevYear}";
+            return $"0+{daysInPrevMonth}. 0+{prevMonth}. {prevYear}";
         }
     }
 }
